c4c5c452e285ca6e80e58ac94dafd6b97cb7bfff
joseterk <joseterk@hotmail.com>
2021-05-16 17:38:44 +0200

    Agregar archivos de proyecto.

---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IntercambioDeIdiomas
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ProbarBD();
+        }
+
+        private void ProbarBD()
+        {
+
+            using (IntercambioIdiomasEntities db= new IntercambioIdiomasEntities())
+            {
+                var listaUsuario = db.USUARIO.ToList();
+
+                foreach(var user in listaUsuario)
+                {
+                }
+
+                IdDatagrid.DataSource = listaUsuario;
+                IdDatagrid.DataBind();
+
+            }
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntercambioDeIdiomas.Global" Language="C#" %>
