ef688e837f3c73437d4e4702d3007370c2b4fa03
joseterk <joseterk@hotmail.com>
2021-05-18 18:15:00 +0200

    subida 1 cambio bootstrap y Crear perfil

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,7 +11,8 @@ namespace IntercambioDeIdiomas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            ProbarBD();
+            Response.Redirect("CrearPerfil.aspx",true);
+            //ProbarBD();
         }
 
         private void ProbarBD()
